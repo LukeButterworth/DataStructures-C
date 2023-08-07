e79c345617527c74da092b89e4d4353da63cb414
LukeButterworth <luke.butterworth@protonmail.com>
2023-08-07 18:52:52 +1000

    Started Implementation of Linked List

---
+++ src/linked-list/main.c
@@ -0,0 +1,71 @@
+#include <stdlib.h>
+#include <stdint.h>
+
+//Enum Definitions
+//This is not an elegant way of defining generics, I've seen methods that use macros
+typedef enum {
+	LINKED_LIST_INT,
+	LINKED_LIST_BOOL,
+	LINKED_LIST_CHAR
+} DataType;
+
+//Struct Declarations
+typedef struct Node Node;
+typedef struct LinkedList LinkedList;
+
+//Function Declarations
+//Node Functions
+Node new_node(void *data);
+
+//LinkedList Functions
+LinkedList new_linked_list(DataType type);
+void add(LinkedList list, void *value);
+void remove(LinkedList list, );
+void push(LinkedList list, );
+void pop(LinkedList list, void *value);
+
+//Struct Definitions
+typedef struct Node {
+	void *data;	
+	Node *linked_node;
+} Node;
+
+typedef struct LinkedList {
+	DataType type;
+	size_t length;
+	Node *initial_node;
+	Node *final_node;
+} LinkedList;
+
+//Linked List Functions
+LinkedList new_linked_list(DataType type) {
+	LinkedList list = {
+		.type = type,
+		.length = 0,
+	};
+
+	return list;
+}; 
+
+void add(LinkedList list, void *value) {
+	DataType type = list.type;
+	//we need to perform some type verification here
+	switch (list.type)
+	{
+		case DataType.
+	}
+	
+	Node new_node;
+
+	list.length++;
+};
+
+void push(LinkedList list, void *value) {
+	DataType type = list.type;
+	list.length++
+};
+
+void pop(LinkedList list, void *value) {
+};
+
+//Node Functions
